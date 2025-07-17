3c32f8310fad1bc8bf2f22d14bf26df47694ed22
Sushmita Ghosh <sushmitag464@gmail.com>
2025-07-17 17:11:16 +0530

    Fix : Corrected case

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const { required } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-Mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
     email : {
